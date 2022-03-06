f8ea7a652a142908b4d9aa71dac018ad6fa32215
Tanjil Pranto <tanjil.hossain@metadesignsolutions.co.uk>
2022-03-06 14:39:55 +0600

    added styles to Cardcomponent

--- Components/CardComp.js
+++ Components/CardComp.js
@@ -1,71 +1,84 @@
 import React from "react";
-import { StyleSheet, Text, Button, View ,ScrollView} from "react-native";
+import { StyleSheet, Text, Button, View, ScrollView } from "react-native";
 import { Avatar, Card, Title, Paragraph } from "react-native-paper";
 
-function CardComp({ color }) {
+function CardComp() {
   return (
-    <View style={{ margin: 10 }}>
+    <View style={styles.Container}>
       <Card>
-       <View style={{flexDirection:'row'}}>
-       <Card.Cover
-          style={{ resizeMode: "cover", height: 100, width: "40%" }}
-          source={{ uri: "https://picsum.photos/701" }}
-        />
-       <ScrollView style={{backgroundColor:'red',width:'100%',height:100}}>
-          <Text style={{fontSize:15,fontWeight:'bold'}}>Title for Product</Text>
-          <Paragraph>Product Details</Paragraph>
-        </ScrollView>
-       </View>
-        
-        <View style={{ flexDirection: "row",marginTop:5,alignItems:'center',justifyContent:'space-between' }}>
-          
-          <View style={{ flexDirection: "row", alignItems: "center"}}>
-          <View
-            style={{
-              backgroundColor: "black",
-              shadowColor: "#000",
-              shadowOffset: { width: 0, height: 1 },
-              shadowOpacity: 0.8,
-              shadowRadius: 2,
-              elevation: 5,
-              borderRadius: 10,
-              height:40,
-              width:30
-            }}
-          >
-            <Button
-              color="white"
-              title="+"
-              onPress={() => console.log("Increment")}
-            ></Button>
-          </View>
+        <View style={styles.Content}>
+          <Card.Cover
+            style={styles.image}
+            source={{ uri: "https://picsum.photos/701" }}
+          />
+          <ScrollView style={styles.ProductDetails}>
+            <Text style={{ fontSize: 15, fontWeight: "bold" }}>
+              Title for Product
+            </Text>
+            <Paragraph>Product Details</Paragraph>
+          </ScrollView>
+        </View>
 
-          <Text style={{ padding: 10 }}>5</Text>
-          <View
-            style={{
-              backgroundColor: "black",
-              shadowColor: "#000",
-              shadowOffset: { width: 0, height: 1 },
-              shadowOpacity: 0.8,
-              shadowRadius: 2,
-              elevation: 5,
-              borderRadius: 10,
-              height:40,
-              width:30
-            }}
-          >
-            <Button
-              color="white"
-              title="-"
-              onPress={() => console.log("Decrement")}
-            ></Button>
+        <View style={styles.BottomButtonView}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <View style={styles.ButtonContainer}>
+              <Button
+                color="white"
+                title="+"
+                onPress={() => console.log("Increment")}
+              ></Button>
+            </View>
+
+            <Text style={{ padding: 10 }}>5</Text>
+            <View style={styles.ButtonContainer}>
+              <Button
+                color="white"
+                title="-"
+                onPress={() => console.log("Decrement")}
+              ></Button>
+            </View>
           </View>
-          </View>
-          <Text style={{paddingRight:10}}>$10</Text>
+          <Text style={{ paddingRight: 10 }}>$10</Text>
         </View>
       </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  Container: {
+    margin: 10,
+  },
+  Content: {
+    flexDirection: "row",
+  },
+  image: {
+    resizeMode: "cover",
+    height: 100,
+    width: "40%",
+  },
+  ProductDetails: {
+    backgroundColor: "red",
+    width: "100%",
+    height: 100,
+  },
+  BottomButtonView: {
+    flexDirection: "row",
+    marginTop: 5,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  ButtonContainer: {
+    backgroundColor: "black",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+    borderRadius: 10,
+    height: 40,
+    width: 30,
+  },
+});
 
 export default CardComp;
